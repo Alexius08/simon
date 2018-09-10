15d6c37c63d0b42ccfbb35803de57dd530095d06
Alexius08 <jrsfac@gmail.com>
2018-09-10 22:25:18 +0800

    Modernize code
    Begin removing bits of jQuery, adopt ES6 style

--- js/index.js
+++ js/index.js
@@ -1,14 +1,14 @@
-var level=0, part, current=0, isDone=true, isStrict=false;
-var audiosource="https://s3.amazonaws.com/freecodecamp/simonSound"
-var buttonNo=[{highColor:"#f00",lowColor:"#faa",audio:new Audio(audiosource+"1.mp3")},
+let level=0, part, current=0, isDone=true, isStrict = false;
+const audiosource="https://s3.amazonaws.com/freecodecamp/simonSound";
+const buttonNo=[{highColor:"#f00",lowColor:"#faa",audio:new Audio(audiosource+"1.mp3")},
               {highColor:"#ff0",lowColor:"#ffa",audio:new Audio(audiosource+"2.mp3")},
               {highColor:"#0f0",lowColor:"#afa",audio:new Audio(audiosource+"3.mp3")},
-              {highColor:"#00f",lowColor:"#aaf",audio:new Audio(audiosource+"4.mp3")}]
+              {highColor:"#00f",lowColor:"#aaf",audio:new Audio(audiosource+"4.mp3")}];
 
 $(document).ready(function(){
 	$("#startgame").click(function(){newGame()});
   $("#strictmode").click(function(){
-    isStrict=!isStrict;
+    isStrict = !isStrict;
   });
 	$(".choices").click(function(){
 		if(isDone){
@@ -20,37 +20,35 @@ $(document).ready(function(){
             level++;
             showSequence();
           }
-          else setTimeout(function(){$("#startgame").html("Play again")},500);
+          else setTimeout(()=>{$("#startgame").html("Play again")},500);
         }
       }
       else{
         //insert reaction to erroneous input here
-        var blinker=0;
-        var blinkPanel=setInterval(function(){
-          $("#menupanel").css("background-color","#f88");
-          setTimeout(function(){$("#menupanel").css("background-color","#ddd");},500);
+        let blinker=0;
+        let blinkPanel = setInterval(()=>{
+          document.getElementById("menupanel").style.backgroundColor = "#f88";
+          setTimeout(()=>{document.getElementById("menupanel").style.backgroundColor = "#ddd";},500);
           blinker++;
-          if(blinker==3){
-            clearInterval(blinkPanel);
-          }
+          if(blinker==3) clearInterval(blinkPanel);
         },1000);
-        if(isStrict) setTimeout(function(){newGame();},3000);
-        else setTimeout(function(){showSequence();},3000);
+        if(isStrict) setTimeout(()=>{newGame();},3000);
+        else setTimeout(()=>{showSequence();},3000);
       }
     }
 	});
 });
 
 function showSequence(){
-	isDone=false;
-	current=0;
-	var ctr2=0;
-	var display=setInterval(function(){
-		$("#level").html(level);
+	isDone = false;
+	current = 0;
+	let ctr2 = 0;
+	let display = setInterval( () => {
+		document.getElementById("level").innerHTML = level;
 		blinkButton(part[ctr2]);
 		ctr2++;
-		if(ctr2>=level){
-			isDone=true;
+		if(ctr2 >= level){
+			isDone = true;
 			clearInterval(display);
 		}
 	},1000);
@@ -58,17 +56,17 @@ function showSequence(){
 
 function blinkButton(num){
   buttonNo[num].audio.play();
-	$("#choice"+num).css("background-color",buttonNo[num].highColor);
-	setTimeout(function(){$("#choice"+num).css("background-color",buttonNo[num].lowColor);},500);
+  document.getElementById(`choice${num}`).style.backgroundColor = buttonNo[num].highColor;
+	setTimeout(()=>{document.getElementById(`choice${num}`).style.backgroundColor = buttonNo[num].lowColor;},500);
 }
 
 function newGame(){
-  $("#startgame").html("Restart");
+  document.getElementById("startgame").innerHTML = "Restart";
   part=[];
-  for(var ctr=0;ctr<20;ctr++){
+  for(let ctr = 0; ctr<20 ;ctr++){
     part.push(Math.floor(4*Math.random()));
   }
   level=1;
-  $("#level").html(1);
+  document.getElementById("level").innerHTML = 1;
   showSequence();
 }
